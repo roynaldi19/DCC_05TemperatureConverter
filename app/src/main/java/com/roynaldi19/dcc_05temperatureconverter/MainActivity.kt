617b767634bd33ab5103215d3dc7d514beb5a663
roynaldi19 <roynaldi19@gmail.com>
2024-02-10 20:13:13 +0700

    statefull dan stateless converter

---
+++ app/src/main/java/com/roynaldi19/dcc_05temperatureconverter/MainActivity.kt
@@ -0,0 +1,136 @@
+package com.roynaldi19.dcc_05temperatureconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.roynaldi19.dcc_05temperatureconverter.ui.theme.DCC_05TemperatureConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DCC_05TemperatureConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        StatefulTemperatureInput()
+                        ConvertApp()
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun StatefulTemperatureInput(modifier: Modifier = Modifier) {
+    var input by remember {
+        mutableStateOf("")
+    }
+    var output by remember {
+        mutableStateOf("")
+    }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+
+    }
+}
+
+fun convertToFahrenheit(celsius: String) =
+    celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
+
+@Composable
+fun StatelessTemperatureInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.padding(16.dp)) {
+        Text(
+            stringResource(R.string.stateless_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+
+    }
+}
+
+@Composable
+private fun ConvertApp(
+    modifier: Modifier = Modifier
+) {
+    var input by remember {
+        mutableStateOf("")
+    }
+    var output by remember {
+        mutableStateOf("")
+    }
+    Column(modifier) {
+        StatelessTemperatureInput(input = input, output = output, onValueChange = { newInput ->
+            input = newInput
+            output = convertToFahrenheit(newInput)
+        })
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DCC_05TemperatureConverterTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column {
+                StatefulTemperatureInput()
+                ConvertApp()
+            }
+
+        }
+    }
+}
